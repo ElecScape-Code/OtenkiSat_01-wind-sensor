7ac8c0748e7cd7e88762a4cb5e514c3e05d5b5af
sonenagisa <93703736+sonenagisa@users.noreply.github.com>
2025-02-11 15:40:38 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,57 @@
-#include "mbed.h"
+/**
+  @file main.cpp
+  @brief This is the sample code for FS3000 wind velocity sensor.
+  @date 2024/08/21
+  @ref https://www.renesas.com/us/en/document/dst/fs3000-datasheet?r=1395551
 
-DigitalOut myled(LED1);
+*/
+
+
+#include "mbed.h"
+#include <cstdint>
+#include <ctime>
+
+RawSerial pc(USBTX,USBRX,9600);
+
+I2C fs3000(I2C_SDA, I2C_SCL);
+
+#define FS3000_DEVICE_ADDRESS 0x28 << 1
+#define FS3000_DATA_LENGTH    5
+
+#define FS3000_HIGH_BYTE_MASK 0b00001111
+
+uint16_t read_data();
+float get_wind_velocity();
 
 int main() {
   while(1) {
-    myled = 1;
-    wait(0.2);
-    myled = 0;
-    wait(0.2);
+    float wind_speed = get_wind_velocity();
+    pc.printf("wind speed: %.2f [m/s]\r\n", wind_speed);
+    wait(1);
+  }
+}
+
+uint16_t read_raw_data() {
+  char rx_buf[FS3000_DATA_LENGTH] = {0x00};
+
+  fs3000.read(FS3000_DEVICE_ADDRESS, rx_buf, FS3000_DATA_LENGTH);
+
+  uint8_t high_byte = rx_buf[1];
+  uint8_t low_byte = rx_buf[2] & FS3000_HIGH_BYTE_MASK;
+
+  return ((high_byte << 8) | low_byte);
+}
+
+float get_wind_velocity() {
+  uint16_t raw_val = read_raw_data();
+
+  // pc.printf("wind speed: %d\r\n", raw_val);
+
+  float wind_speed = raw_val * 0.0022 - 1.1685;
+
+  if(wind_speed > 0) {
+    return wind_speed;
+  } else {
+    return 0;
   }
 }
